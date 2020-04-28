9ec7f1a03bf4408d01705bac15a7acaaa85cb96c
tac0de <wonyoungchoiseoul@gmail.com>
2020-04-28 22:12:54 +0900

    finished.. some animations left

--- src/App.js
+++ src/App.js
@@ -1,21 +1,76 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { Route } from "react-router-dom";
+import gsap from "gsap";
 import "./styles/App.scss";
+
+// components
 import Header from "./components/Header";
-import Banner from "./components/Banner";
-import Cases from "./components/Cases";
+import Navigation from "./components/Navigation";
+
+// pages
+import Home from "./pages/Home";
+import CaseStudies from "./pages/CaseStudies";
+import Approach from "./pages/Approach";
+import Services from "./pages/Services";
+import About from "./pages/About";
+
+const routes = [
+  { path: "/", name: "Home", Component: Home },
+  { path: "/case-studies", name: "Case Studies", Component: CaseStudies },
+  { path: "/approach", name: "Approach", Component: Approach },
+  { path: "/services", name: "Services", Component: Services },
+  { path: "/about-us", name: "About Us", Component: About },
+];
+
+function debounce(fn, ms) {
+  let timer;
+  return () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      timer = null;
+      fn.apply(this, arguments);
+    }, ms);
+  };
+}
 
 function App() {
-  useEffect(() => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-  }, []);
+  // prevents flashing
+  gsap.to("body", 0, { css: { visibility: "visible" } });
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
 
+  useEffect(() => {
+    let vh = dimensions.height * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    const debouncedHandleResize = debounce(function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      });
+    }, 1000);
+
+    window.addEventListener("resize", debouncedHandleResize);
+
+    return () => {
+      window.removeEventListener("resize", debouncedHandleResize);
+    };
+  });
   return (
-    <div className="App">
-      <Header />
-      <Banner />
-      <Cases />
-    </div>
+    <>
+      <Header dimensions={dimensions} />
+
+      <div className="App">
+        {routes.map(({ path, Component }) => (
+          <Route key={path} exact path={path}>
+            <Component dimensions={dimensions} />
+          </Route>
+        ))}
+      </div>
+      <Navigation />
+    </>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import { ReactComponent as UpArrow } from "../assets/up-arrow-circle.svg";
+import { openMenu, closeMenu } from "../animations/menuAnimations";
 
-const Header = () => {
+const Header = ({ history, dimensions }) => {
+  const [menuState, setMenuState] = useState({ menuOpened: false });
+
+  useEffect(() => {
+    //Listening for page changes.
+
+    if (menuState.menuOpened === true) {
+      openMenu(dimensions.width);
+    } else if (menuState.menuOpened === false) {
+      closeMenu();
+    }
+  });
+
   return (
     <div className="header">
       <div className="container">
         <div className="row v-center space-between">
           <div className="logo">
-            <a href="/">AGENCY.</a>
+            <NavLink to="/">AGENCY.</NavLink>
           </div>
-          <div className="nav">
-            <span></span>
-            <span></span>
+          <div className="nav-toggle">
+            <div
+              onClick={() => setMenuState({ menuOpened: true })}
+              className="hamburger-menu"
+            >
+              <span></span>
+              <span></span>
+            </div>
+            <div
+              onClick={() => setMenuState({ menuOpened: false })}
+              className="hamburger-menu-close"
+            >
+              <UpArrow />
+            </div>
           </div>
         </div>
       </div>
